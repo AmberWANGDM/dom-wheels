9b1318271f3393232b4f7485a55b96eb8525d6f1
WangDanmeng <WangDanmeng98@163.com>
2022-12-07 00:15:54 +0800

    使用原型提取共用属性

--- dom-jQuery/src/jquery.js
+++ dom-jQuery/src/jquery.js
@@ -14,66 +14,72 @@ window.$ = window.jQuery = function (selectorOrArrayOrTemplate) {
   function createElement(string) {
     const container = document.createElement('template')
     container.innerHTML = string.trim()
-    // console.log(container.content.firstChild)
     return container.content.firstChild
   }
-  // console.log(elements)
-  return {
-    jquery: true,
+
+  const api = Object.create(jQuery.prototype) //api.__proto__ => $.prototype
+  Object.assign(api, {
+    elements: elements,
     oldApi: selectorOrArrayOrTemplate.oldApi,
-    get(index) {
-      return elements[index]
-    },
-    appendTo(node) {
-      if (node instanceof Element) {
-        this.each((el) => node.appendChild(el))
-      } else if (node.jquery) {
-        this.each((el) => node.get(0).appendChild(el))
+  })
+  return api
+}
+
+jQuery.prototype = {
+  constructor: jQuery,
+  jquery: true,
+  get(index) {
+    return this.elements[index] // this 就是 api
+  },
+  appendTo(node) {
+    if (node instanceof Element) {
+      this.each((el) => node.appendChild(el))
+    } else if (node.jquery) {
+      this.each((el) => node.get(0).appendChild(el))
+    }
+  },
+  addClass(className) {
+    for (let i = 0; i < this.elements.length; i++) {
+      elements[i].classList.add(className)
+    }
+    return this // 链式
+  },
+  find(selector) {
+    let arr = []
+    for (let i = 0; i < this.elements.length; i++) {
+      let elements2 = this.elements[i].querySelectorAll(selector)
+      arr.push(...elements2)
+    }
+    arr.oldApi = this
+    let newApi = jQuery(arr)
+    return newApi
+  },
+  end() {
+    return this.oldApi
+  },
+  each(fn) {
+    for (let i = 0; i < this.elements.length; i++) {
+      fn.call(undefined, this.elements[i], i)
+    }
+    return this
+  },
+  parent() {
+    let arr = []
+    this.each((node) => {
+      if (arr.indexOf(node.parentNode) === -1) {
+        arr.push(node.parentNode)
       }
-    },
-    addClass(className) {
-      for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.add(className)
-      }
-      return this // 链式
-    },
-    find(selector) {
-      let arr = []
-      for (let i = 0; i < elements.length; i++) {
-        let elements2 = elements[i].querySelectorAll(selector)
-        arr.push(...elements2)
-      }
-      arr.oldApi = this
-      let newApi = jQuery(arr)
-      return newApi
-    },
-    end() {
-      return this.oldApi
-    },
-    each(fn) {
-      for (let i = 0; i < elements.length; i++) {
-        fn.call(undefined, elements[i], i)
-      }
-      return this
-    },
-    parent() {
-      let arr = []
-      this.each((node) => {
-        if (arr.indexOf(node.parentNode) === -1) {
-          arr.push(node.parentNode)
-        }
-      })
-      return jQuery(arr)
-    },
-    children() {
-      let arr = []
-      this.each((node) => {
-        arr.push(...node.children)
-      })
-      return jQuery(arr)
-    },
-    print() {
-      console.log(elements)
-    },
-  }
+    })
+    return jQuery(arr)
+  },
+  children() {
+    let arr = []
+    this.each((node) => {
+      arr.push(...node.children)
+    })
+    return jQuery(arr)
+  },
+  print() {
+    console.log(this.elements)
+  },
 }
